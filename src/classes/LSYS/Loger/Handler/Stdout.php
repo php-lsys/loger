583673ef66a79a806bce800847437b596fd41e6b
shanliu <shan.liu@msn.com>
2019-03-20 15:08:32 +0800

    console output

--- src/classes/LSYS/Loger/Handler/Stdout.php
+++ src/classes/LSYS/Loger/Handler/Stdout.php
@@ -10,15 +10,10 @@ use LSYS\Loger\Handler;
 use LSYS\Loger;
 use LSYS\Loger\Format;
 use LSYS\Loger\Format\Trace;
-use LSYS\Exception;
-class Folder implements Handler
+class Stdout implements Handler
 {
-	//save log dir
-    protected $_folder;
     //handler level
     protected $_level;
-    //save file ext
-    protected $_ext;
     /**
      * @var Format
      */
@@ -30,38 +25,9 @@ class Folder implements Handler
      * @param int      $level          The minimum logging level at which this handler will be triggered
      * @param Boolean  $bubble         Whether the messages that are handled can bubble up the stack or not
      */
-    public function __construct($folder,$level = Loger::DEBUG,$ext='.log')
+    public function __construct($level = Loger::DEBUG)
     {
-        $this->_folder = rtrim($folder,'/\\').'/';
         $this->_level=$level;
-        $this->_ext=$ext;
-    }
-    /**
-     * Return the currently active stream if it is open
-     *
-     * @return resource|null
-     */
-    private function _file()
-    {
-    	if ($this->_file&&$this->_time>=strtotime("today")) return $this->_file;
-    	// may be more to : __construct ??
-    	if (!is_writable($this->_folder)) throw new Exception("can't be write to :".$this->_folder);
-    	// Set the yearly directory name
-    	$directory = $this->_folder.date('Ym');
-    	
-    	if ( ! is_dir($directory))
-    	{
-    		// Create the yearly directory
-    		mkdir($directory, 02777);
-    		
-    		// Set permissions (must be manually set to fix umask issues)
-    		chmod($directory, 02777);
-    	}
-    	// Set the name of the log file
-    	$filename = $directory.DIRECTORY_SEPARATOR.date('d').$this->_ext;
-		$this->_file=$filename;
-		$this->_time=time();
-    	return $filename;
     }
     /**
      * {@inheritDoc}
@@ -84,8 +50,8 @@ class Folder implements Handler
      */
     public function handle(array $record){
     	$msg=$this->getFormat()->format($record);
-    	$filename=$this->_file();
-    	return file_put_contents($filename, $msg, FILE_APPEND);
+    	print $msg;
+    	flush();
     }
     /**
      * {@inheritDoc}
@@ -97,7 +63,8 @@ class Folder implements Handler
     	foreach ($records as $record){
     		$msg.=$format->format($record);
     	}
-    	return file_put_contents($this->_file(), $msg, FILE_APPEND);
+    	print $msg;
+    	flush();
     }
     /**
      * {@inheritDoc}
